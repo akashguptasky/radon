01555d3abda19a773ae707bb1d8d1def76df815f
akashguptasky <skygupta193@gmail.com>
2022-06-03 13:39:37 +0530

    This is Preetesh sir assignment Date:3/6/20222

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,89 +1,63 @@
 const express = require('express');
-const myHelper = require('../util/helper')
 const underscore = require('underscore')
-
 const router = express.Router();
 
+let players =
+[
 
+    {
+        "name":"Cristiano Ronaldo",
+        "dob":"1/2/1985",
+        "gender":"Male",
+        "city":"Funchal",
+        "sports":["Football"]
+    },
 
-// first movies api 
-router.get('/movies', function (req, res) {
-   let moviesArr=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins'];
-   //seperate
-    res.send(moviesArr.join(","));
-    console.log(...moviesArr)
-});
+    {
+        "name":"Lionel Messi",
+        "dob":"24/6/1987",
+        "gender":"Male",
+        "city":"Argentina",
+        "sports":["Footwbll"]
+    },
 
-// second find movies by using index 
-router.get('/movies/:indexNumber',function(req,res){
-let moviesArr=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins'];
-let index=req.params.indexNumber;
-if(index<=moviesArr.length-1)
-{
-    res.send( moviesArr[index]);
-}
-// handle problem 
-else
-{
-    res.send("sorry Availalbe movies index only 0 to 3. so plz select only valid number");
-}
-
-});
-
-// create films api here 
-router.get('/films',function(req,res){
-  const myObjFilmArr=  [ {
-        'id': 1,
-        'name': 'The Shining'
-       }, {
-        'id': 2,
-        'name': 'Incendies'
-       }, {
-        'id': 3,
-        'name': 'Rang de Basanti'
-       }, {
-        'id': 4,
-        'name': 'Finding Nemo'
-       }]
-
-       res.send(myObjFilmArr);
-});
-
-
-
-router.get('/films/:filmId',function(req,res){
-    const myObjFilmArr=  [ {
-        'id': 1,
-        'name': 'The Shining'
-       }, {
-        'id': 2,
-        'name': 'Incendies'
-       }, {
-        'id': 3,
-        'name': 'Rang de Basanti'
-       }, {
-        'id': 4,
-        'name': 'Finding Nemo'
-       }]
-       let index=req.params.filmId;
-       if(index>myObjFilmArr.length)
-       {
-        res.send("No movie exists with this id")
-       }
-       else
-       { 
-      for(let i=0;i<myObjFilmArr.length;i++)
-      {
-          if(myObjFilmArr[i].id==index)
-          {
-           res.send("Film Name is: "+myObjFilmArr[i].name +"  and Objec is : "+JSON.stringify(myObjFilmArr[i]));
-            
-          }
-      }
+    {
+        "name":"Virat Kohli",
+        "dob":"5/11/1988",
+        "gender":"Male",
+        "city":"Delhi",
+        "sports":["Cricket"]
     }
 
-});
+]
 
+router.post('/players',function(req,res){
+
+   
+    let obj={
+        "name":req.body.name,
+        "dob":req.body.dob,
+        "gender":req.body.gender,
+        "city":req.body.city,
+        "sports":req.body.sports
+        
+    }
+    let b='false';
+   for(let i=0;i<players.length;i++)
+   {
+       if(players[i].name==req.body.name)
+       {
+        b='true';
+        break;
+       }
+   }
+    if(b=='false')
+    {
+        players.push(obj);
+    }
+   
+    res.send(players);
+});
 
 module.exports = router;
 // adding this comment for no reason
