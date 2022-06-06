f602fe0bf87aaeffe2e182e3191b6a81269e1c63
akashguptasky <skygupta193@gmail.com>
2022-06-07 01:18:55 +0530

    my Books Assignment 7-6-2022

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -2,30 +2,22 @@ const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
     bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
     prices: {
         indianPrice: String,
-        europePrice: String,
+        europePrice: String
     },
-    sales: {type: Number, default: 10}
+    year:{
+        type:Number,
+        default:2021
+    },
+    tags:[String],
+    authorName: String, 
+    totalPages:Number,
+    stock:{
+        type:Boolean,
+        default:false
+    }
 }, { timestamps: true });
 
 
 module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
